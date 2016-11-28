ae1090eeb3fa3d84300fb69a1ebfbe5424304265
Andrew Pau <apau4180@berkeley.edu>
2016-11-28 12:35:17 -0800

    Finished working draft of website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+    $(".navbarItem").click(function() {
+        var id = "#" + $(this).text();
+        var top = $(id).offset().top;
+        $('html,body').animate({scrollTop: top - 50}, 500);
+    });
+
+    $(".media").hover(function() {
+        var media = "." + this.id;
+        console.log(media);
+        $(media).stop(true, true).fadeIn(400);
+    }, function() {
+        var media = "." + this.id;
+        $(media).stop(true, true).fadeOut(400);
+    });
+
 });
